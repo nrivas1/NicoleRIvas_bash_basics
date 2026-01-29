c7759b1d8ab70517c0debfe3840bc91f3f55baa2
Nicole Rivas <nrivas1@ggc.edu>
2026-01-29 15:32:32 -0500

    I have added my name

---
+++ NicoleRivas/print_name.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Nicole Rivas"
+
+echo "Hello Steven Lopez!"
+echo "How old are you?" 
+read age
+echo 'Thanks Steven! I see you are ' $age ' years old'
+
+echo "What school do you currently attend?"
+read school
+echo 'Nice! ' $school ' sounds like a great place to learn.'
